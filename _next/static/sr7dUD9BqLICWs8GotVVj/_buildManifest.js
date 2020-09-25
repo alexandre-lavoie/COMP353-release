394aae493ccb7202e784b949afb6b089b9f2a7e2
Alexandre Lavoie <alexandre.lavoie00@gmail.com>
2020-09-24 21:15:13 -0400

    Release

---
+++ _next/static/sr7dUD9BqLICWs8GotVVj/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-bd43a118efa0afbaab2d.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-b45c47a795a8e787c08c.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
